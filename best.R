95e11e714b259eb6802a927ab426055f9aec2ce9
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-06-04 19:00:01 +0530

    Added input validity checks for state and outcome variable

--- best.R
+++ best.R
@@ -1,5 +1,22 @@
 best <- function(state,outcome){
-  outcome <- read.csv('outcome-of-care-measures.csv')
-  statedata <- split(outcome,outcome$State)$state
+  # --------- Checking for valid input in outcome -------
+  
+  check <- c("heart attack", "“heart failure", "pneumonia")
+  if (is.null(outcome)) {
+    message("invalid outcome")
+  }
+  if (!outcome %in% check) stop("invalid outcome")
+  
+  # -------- Checking for valid input of state -----------
+  data1 <- read.csv('outcome-of-care-measures.csv')
+  stateNames <- unique(data1$State)
+  if (is.null(state)) {
+    message("invalid state")
+  }
+  if (!state %in% stateNames) stop("invalid state")
+  
+  
+  
+  statedata <- split(data1,data1$State)[[state]]
   
 }
