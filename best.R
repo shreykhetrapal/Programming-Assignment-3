9779e3bdb55b78d7f5cf2ec5a43e5b99b84b930e
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-06-05 11:33:12 +0530

    Arranged in a better way

--- best.R
+++ best.R
@@ -1,4 +1,5 @@
 best <- function(state,outcome){
+  
   # --------- Checking for valid input in outcome -------
   
   check <- c("heart attack", "heart failure", "pneumonia")
@@ -7,7 +8,7 @@ best <- function(state,outcome){
   }
   if (!outcome %in% check) stop("invalid outcome")
   
-  # -------- Checking for valid input of state -----------
+  # --------- Checking for valid input of state -----------
   data1 <- read.csv('outcome-of-care-measures.csv')
   stateNames <- unique(data1$State)
   if (is.null(state)) {
@@ -15,13 +16,17 @@ best <- function(state,outcome){
   }
   if (!state %in% stateNames) stop("invalid state")
   
-  # ------ Setting outcome to column names --------
+  # ------ Spitting dataframe according to state
+  
+  statedata <- split(data1,data1$State)[[state]]
+  
+  # --------- Setting outcome to column names --------
   
   if(outcome=="heart attack") outcome = statedata$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack
   if(outcome=="heart failure") outcome = statedata$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
   if(outcome=="pneumonia") outcome = statedata$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia
   
-  statedata <- split(data1,data1$State)[[state]]
+  # --------- Subsetting required conditions ---------
   
   best_result <- statedata$Hospital.Name[which.min(as.numeric(outcome))]
   
